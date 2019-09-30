98a0b351f9294ec5b9ab6bee5225d56fe483d5b4
Yahali Sherman <ysherman@heed-global.com>
2019-09-30 23:50:22 +0300

    update repository link.
    Signed-off-by: Yahali Sherman <ysherman@heed-global.com>

--- contendo_utils/contendo_utils/setup.py
+++ contendo_utils/contendo_utils/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='contendo_utils',
       version='0.2.11',
       description='General python and google-cloud related utilities',
-      url='https://github.com/sportsight-pro/sportsight-core/tree/master/contendo_utils',
+      url='https://github.com/Yahalis/contendo_core1/tree/master/contendo_utils',
       author='Yahali Sherman, contendo.ai',
       author_email='yahali@contendo.ai',
       license='MIT',
