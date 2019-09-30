386f178ee716e4ba009c11240e65ddad99bfe362
Yahali Sherman <ysherman@heed-global.com>
2019-09-30 23:47:42 +0300

    Add support for index fundamentals import (GSPC &DJI) Various small fixes
    Signed-off-by: Yahali Sherman <ysherman@heed-global.com>

---
+++ contendo_utils/contendo_utils/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='contendo_utils',
+      version='0.2.11',
+      description='General python and google-cloud related utilities',
+      url='https://github.com/sportsight-pro/sportsight-core/tree/master/contendo_utils',
+      author='Yahali Sherman, contendo.ai',
+      author_email='yahali@contendo.ai',
+      license='MIT',
+      packages=['contendo_utils'],
+      install_requires=[
+          'gcsfs',
+          'google-cloud-bigquery',
+          'google-cloud-storage',
+      ],
+      zip_safe=False)
